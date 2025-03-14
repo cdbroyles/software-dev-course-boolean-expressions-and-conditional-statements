b178aeb0e5c563c4584831fbf622f47fe1cfa3de
cdbroyles <cdbroyles1986@gmail.com>
2025-03-14 10:00:48 -0500

    Finished assignment

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,20 +28,43 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+let location = null;
+let openChest = null;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+const choice = readline.question("Do you go to the 'mountains' or the 'village'? ").toLowerCase();
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
+  location = "through the mountains";
 } else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
+} else if (choice === "village" && hasMap) {
   console.log("You find your way to the village.");
-} else {
-  console.log("You get lost and wander aimlessly.");
+} else if (choice === "village" && !hasMap) {
+  console.log("You get lost, wander aimlessly, and die.");
 }
 
+if (location === "through the mountains") {
+  console.log("You find a treasure chest!");
+  openChest = readline.question("Do you open it? (yes/no)? ").toLowerCase();
+}
+
+if (openChest === "yes") {
+  console.log("You open the chest and find a sword!");
+} else if (openChest === "no") {
+  console.log("You feel as though the chest might be a trick.  You decide to leave it.");
+}
+
+if (openChest) {
+  console.log("An enemy approaches.");
+  fight = readline.question("Do you want to fight (yes/no)? ").toLowerCase();
+  if (fight === "yes" && openChest === "yes") {
+    console.log("You win the battle and live happily ever after!.");
+  } else if (fight === "no" || openChest === "no") {
+    console.log("The enemy overpowers you and you die.");
+  }
+}
 /* 
 
 Add Customization and expand the game:
